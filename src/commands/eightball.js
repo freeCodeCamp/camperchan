7f379de51f3d4f47696a0b30c8a0216b8bf5ff8c
Nicholas Carrigan <63889819+nhcarrigan@users.noreply.github.com>
2020-05-13 01:19:01 -0700

    Set up eightball command
    Created array with the 20 responses from the original Magic 8 Ball

--- src/commands/eightball.js
+++ src/commands/eightball.js
@@ -8,7 +8,11 @@ module.exports = {
    */
   command: function eightBall(message) {
     console.log('eightBall', message);
+    let options = ["As I see it, yes." , "Ask again later." , "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "It is certain.", "It is decidedly so.", "Most likely.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Outlook good.", "Reply hazy, try again.",
+"Signs point to yes.", "Very doubtful.", "Without a doubt.", "Yes.", "Yes - definitely.", "You may rely on it."]
+    let rand = Math.floor(Math.random() * 20)
     // TODO:
-    message.channel.send('magic not ready yet');
+    message.channel.send(options[rand]);
   },
 };
+
