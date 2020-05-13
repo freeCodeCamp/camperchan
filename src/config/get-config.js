1976d51102cad45db5d0c62e4616358373f94ff0
Christian John Canlas <cjcanlas78@gmail.com>
2020-05-14 01:52:56 +0800

    Call dotenv dependency on get-config.js (#13)
    - Add missing dotenv call, in `get-config`
    
    Closes #2

--- src/config/get-config.js
+++ src/config/get-config.js
@@ -5,6 +5,8 @@
  * @returns {Promise<object>} a promise, to support more complex configs
  * in the future potentially, like loading from a file async, or http.
  */
+require('dotenv').config();
+
 module.exports = function getConfig() {
   return Promise.resolve({
     TOKEN: process.env.TOKEN,
