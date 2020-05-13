a015337c573c4492ac75133fb2d3c0a3b4730224
Nicholas Carrigan <63889819+nhcarrigan@users.noreply.github.com>
2020-05-13 00:17:29 -0700

    Add welcome/depart messages
    Did some research on how to write this - I assume it goes in the same file where message commands are read?

--- src/commands/bootstrap.js
+++ src/commands/bootstrap.js
@@ -5,7 +5,15 @@ const eightball = require('./eightball');
  */
 module.exports = function bootstrap(client) {
   const commands = [eightball];
-
+  
+  client.on('guildMemberAdd', member => {
+    member.guild.channels.get('channelID').send("**" + member.user.username + "** has joined the server! :)")
+  })
+            
+  client.on('guildMemberRemove', member => {
+    member.guild.channels.get('channelID').send("**" + member.user.username + "** has left the server! :(")
+  })
+  
   client.on('message', (message) => {
     for (let command of commands) {
       if (message.content.startsWith(command.prefix)) {
