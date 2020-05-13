5e447e2b29e27a16e17746292587feb9912e8d5b
Nicholas Carrigan <63889819+nhcarrigan@users.noreply.github.com>
2020-05-13 10:54:42 -0700

    Update eightball.js
    Applying recommended changes

--- src/commands/eightball.js
+++ src/commands/eightball.js
@@ -8,10 +8,9 @@ module.exports = {
    */
   command: function eightBall(message) {
     console.log('eightBall', message);
-    let options = ["As I see it, yes." , "Ask again later." , "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "It is certain.", "It is decidedly so.", "Most likely.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Outlook good.", "Reply hazy, try again.",
+    const options = ["As I see it, yes." , "Ask again later." , "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "It is certain.", "It is decidedly so.", "Most likely.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Outlook good.", "Reply hazy, try again.",
 "Signs point to yes.", "Very doubtful.", "Without a doubt.", "Yes.", "Yes - definitely.", "You may rely on it."]
-    let rand = Math.floor(Math.random() * 20)
-    // TODO:
+    const rand = Math.floor(Math.random() * options.length)
     message.channel.send(options[rand]);
   },
 };
