de9042d9631749895d7205885f3bbcb7abaf1630
Nicholas Carrigan <63889819+nhcarrigan@users.noreply.github.com>
2020-05-13 18:57:29 -0700

    Add welcome DM and departing message announcement
    - Add welcome DM that presents the FCC code of conduct (even tho this bot isn't affiliated :P )
    - Add departing announcement when a user leaves the server.

--- src/commands/bootstrap.js
+++ src/commands/bootstrap.js
@@ -1,12 +1,33 @@
-const eightball = require('./eightball');
-
+const eightball = require("./eightball");
+const help = require("./help");
+const Discord = require("discord.js");
 /**
  * Bootstraps all commands to the client.
  */
 module.exports = function bootstrap(client) {
-  const commands = [eightball];
+  const commands = [eightball, help];
 
-  client.on('message', (message) => {
+  client.on("guildMemberAdd", (member) => {
+    const welcomeEmbed = new Discord.MessageEmbed()
+      .setColor("#0099ff")
+      .setTitle("Welcome!")
+      .setDescription("Thank you for joining our server")
+      .addFields({
+        name: "Rules",
+        value:
+          "Please read our [rules](https://www.freecodecamp.org/news/code-of-conduct/) before posting in the server.",
+      })
+      .setFooter("Thank you and Happy Coding! 😁");
+    member.send(welcomeEmbed);
+  });
+
+  client.on("guildMemberRemove", (member) => {
+    member.guild.channels.cache
+      .find((ch) => ch.name === "introduction")
+      .send("**" + member.user.username + "** has left the server! :(");
+  });
+
+  client.on("message", (message) => {
     for (let command of commands) {
       if (message.content.startsWith(command.prefix)) {
         command.command(message);
@@ -15,3 +36,4 @@ module.exports = function bootstrap(client) {
     }
   });
 };
+
