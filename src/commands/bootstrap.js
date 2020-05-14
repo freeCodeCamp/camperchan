fe78555095e01a80a134555b0698081aebe82c74
Nicholas Carrigan <63889819+nhcarrigan@users.noreply.github.com>
2020-05-13 17:34:17 -0700

    Update bootstrap.js
    Convert welcome message to embed, added link to FCC code of conduct (to demonstrate HTML link - this required it to be an embed), added unicode emoji.
    --added discord requirement as embed wouldn't run without it.

--- src/commands/bootstrap.js
+++ src/commands/bootstrap.js
@@ -1,20 +1,33 @@
-const eightball = require('./eightball');
-
+const eightball = require("./eightball");
+const help = require("./help");
+const Discord = require("discord.js");
 /**
  * Bootstraps all commands to the client.
  */
 module.exports = function bootstrap(client) {
-  const commands = [eightball];
-  
-  client.on('guildMemberAdd', member => {
-        member.send("Welcome to the server! Please make sure you read the rules in our '#welcome' channel before you start chatting!")
-  })
-            
-  client.on('guildMemberRemove', member => {
-    member.guild.channels.cache.find(ch => ch.name === 'introduction').send("**" + member.user.username + "** has left the server! :(")
-  })
-  
-  client.on('message', (message) => {
+  const commands = [eightball, help];
+
+  client.on("guildMemberAdd", (member) => {
+    const welcomeEmbed = new Discord.MessageEmbed()
+      .setColor("#0099ff")
+      .setTitle("Welcome!")
+      .setDescription("Thank you for joining our server")
+      .addFields({
+        name: "Rules",
+        value:
+          "Please read our [rules](https://www.freecodecamp.org/news/code-of-conduct/) before posting in the server.",
+      })
+      .setFooter("Thank you and Happy Coding! 😁");
+    member.send(welcomeEmbed);
+  });
+
+  client.on("guildMemberRemove", (member) => {
+    member.guild.channels.cache
+      .find((ch) => ch.name === "introduction")
+      .send("**" + member.user.username + "** has left the server! :(");
+  });
+
+  client.on("message", (message) => {
     for (let command of commands) {
       if (message.content.startsWith(command.prefix)) {
         command.command(message);
@@ -23,3 +36,4 @@ module.exports = function bootstrap(client) {
     }
   });
 };
+
