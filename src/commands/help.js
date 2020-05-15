43bdcdfd32d318d8eb5348a2f2a88b98bfc68792
Nicholas Carrigan <63889819+nhcarrigan@users.noreply.github.com>
2020-05-15 13:29:13 -0700

    Add async/await
    Did I do this right?

--- src/commands/help.js
+++ src/commands/help.js
@@ -6,39 +6,39 @@ module.exports = {
    * Displays currently available commands.
    *
    * @param {Discord.Message} message the message provided
-   *
    *Use this format to add new commands:
    *{name: "Command Name", value: "Command Description"},
    */
 
-  command: function help(message) {
-    console.log("help", message);
-    const helpEmbed = new Discord.MessageEmbed()
-      .setColor("#0099ff")
-      .setTitle("Bot Information")
-      .setDescription(
-        "Hello! I am a test bot created by brantaniguchi and members of FreeCodeCamp.org to experiment with the process of building a Discord Bot. You can view my source code at https://github.com/bradtaniguchi/discord-bot-test"
-      )
-      .addFields(
-        { name: "\u200B", value: "\u200B" },
-        {
-          name: "Commands",
-          value: "Here are my currently available commands!",
-        },
-        {
-          name: "!eightball <optional string>",
-          value: "Returns a response from a magic pool ball!",
-        },
-        //new command lines go below here.
+  command: async function help(message, error) {
+    //console.log("help", message);
+    try {
+      const helpEmbed = await new Discord.MessageEmbed()
+        .setColor("#0099ff")
+        .setTitle("Bot Information")
+        .setDescription(
+          "Hello! I am a test bot created by brantaniguchi and members of FreeCodeCamp.org to experiment with the process of building a Discord Bot. You can view my source code at https://github.com/bradtaniguchi/discord-bot-test"
+        )
+        .addFields(
+          { name: "\u200B", value: "\u200B" },
+          {
+            name: "Commands",
+            value: "Here are my currently available commands!",
+          },
+          {
+            name: "!eightball <optional string>",
+            value: "Returns a response from a magic pool ball!",
+          },
+          //new command lines go below here.
 
-        //for spacing, leave this one at bottom.
-        { name: "\u200B", value: "\u200B" }
-      )
-      .setFooter("I am not affiliated with FreeCodeCamp in any way.");
+          //for spacing, leave this one at bottom.
+          { name: "\u200B", value: "\u200B" }
+        )
+        .setFooter("I am not affiliated with FreeCodeCamp in any way.");
 
-    message.author
-      .send(helpEmbed)
-      .catch((err) => message.channel.send(err.message));
-    message.channel.send(message.author.tag + ", I've sent you a message!");
+      message.author.send(helpEmbed);
+    } catch (error) {
+      console.log(error);
+    }
   },
 };
