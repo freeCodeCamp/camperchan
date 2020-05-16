6c41c06284251ad70dab3e56e8e2bfcd0eeb6f83
Twaha-Rahman <mahi6703890@gmail.com>
2020-05-16 20:27:08 +0600

    Integrated ESLint and Prettier and re-formatted all the codes

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,20 +2,19 @@ module.exports = {
   env: {
     commonjs: true,
     es6: true,
-    node: true,
+    node: true
   },
-  extends: 'eslint:recommended',
+  extends: 'plugin:prettier/recommended',
   globals: {
     Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    SharedArrayBuffer: 'readonly'
   },
   parserOptions: {
-    ecmaVersion: 11,
+    ecmaVersion: 11
   },
   rules: {
-    indent: ['error', 2],
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
-    semi: ['error', 'always'],
-  },
+    semi: ['error', 'always']
+  }
 };
