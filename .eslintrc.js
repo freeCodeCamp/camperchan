f573cdf90c5a9eb71db7021af39eb55eacdbc6df
Twaha-Rahman <mahi6703890@gmail.com>
2020-05-17 07:53:08 +0600

    Resolved merge conflict due to .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,18 +2,18 @@ module.exports = {
   env: {
     commonjs: true,
     es6: true,
-    node: true,
+    node: true
   },
   extends: 'plugin:prettier/recommended',
   globals: {
     Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    SharedArrayBuffer: 'readonly'
   },
   parserOptions: {
-    ecmaVersion: 11,
+    ecmaVersion: 11
   },
   rules: {
     'linebreak-style': ['error', 'unix'],
-    semi: ['error', 'always'],
-  },
+    semi: ['error', 'always']
+  }
 };
