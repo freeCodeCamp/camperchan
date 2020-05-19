237daef578fdc555e342fe3102d8f5a76dc7095b
Nicholas Carrigan <63889819+nhcarrigan@users.noreply.github.com>
2020-05-18 20:27:31 -0700

    complete next round of comments

--- src/commands/help.js
+++ src/commands/help.js
@@ -14,38 +14,29 @@ module.exports = {
 
   command: async function help(message) {
     //console.log("help", message);
-    try {
-      const helpEmbed = {
-        color: '#0099FF',
-        title: 'Bot Information',
-        description:
-          'Hello! I am a test bot created by bradtaniguchi and members of FreeCodeCamp.org to experiment with the process of building a Discord Bot. You can view my source code at https://github.com/bradtaniguchi/discord-bot-test',
-        fields: [],
-        footer: { text: 'I am not affiliated with FreeCodeCamp in any way.' }
-      };
+    const helpEmbed = {
+      color: '#0099FF',
+      title: 'Bot Information',
+      description:
+        'Hello! I am a test bot created by bradtaniguchi and members of FreeCodeCamp.org to experiment with the process of building a Discord Bot. You can view my source code at https://github.com/bradtaniguchi/discord-bot-test',
+      fields: [],
+      footer: { text: 'I am not affiliated with FreeCodeCamp in any way.' }
+    };
 
-      fsPromises
-        .readdir(__dirname)
-        .then((result) => {
-          for (let i = 0; i < result.length; i++) {
-            const filename = result[i];
-            const lookup = require(`./${filename}`);
-            if (!lookup.prefix || !lookup.description) {
-              continue;
-            } else {
-              const fieldObj = {
-                name: lookup.prefix.substring(0, 255),
-                value: lookup.description.substring(0, 1023)
-              };
-              helpEmbed.fields.push(fieldObj);
-            }
-          }
-        })
-        .catch((error) => console.log(error));
-
-      message.author.send({ embed: helpEmbed });
-    } catch (error) {
-      console.error(error);
-    }
+    fsPromises.readdir(__dirname).then(async function(results) {
+      console.log(results);
+      results.forEach(file => {
+        const filename = file;
+        const lookup = require(`./${filename}`);
+        if (lookup.prefix && lookup.description) {
+          const fieldObj = {
+            name: lookup.prefix.substring(0, 255),
+            value: lookup.description.substring(0, 1023)
+          };
+          helpEmbed.fields.push(fieldObj);
+        }
+      });
+      await message.author.send({ embed: helpEmbed });
+    });
   }
 };
