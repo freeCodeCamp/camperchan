f2d9657a0d6e2ea7f8952e31b5e590a6ba82d4a5
brad taniguchi <bradtaniguchi@gmail.com>
2020-06-06 19:04:34 -0700

    Fix issue with run-time erro related to addFormatting

--- src/commands/bootstrap.js
+++ src/commands/bootstrap.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const fs = require('fs');
-const addFormatting = require('./commands/add-formatting');
+const addFormatting = require('../commands/add-formatting');
 const { thanks } = require('./thanks');
 
 /**
