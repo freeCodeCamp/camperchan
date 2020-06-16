4152978a44eb2bd237e761c876eb9faca595974d
Nicholas Carrigan <63889819+nhcarrigan@users.noreply.github.com>
2020-06-16 08:26:42 -0700

    lint

--- src/commands/bootstrap.js
+++ src/commands/bootstrap.js
@@ -124,7 +124,9 @@ module.exports = function bootstrap({ client, config }) {
     if (message.attachments.array().length > 0) {
       if (!message.attachments.array()[0].height) {
         message.delete();
-        message.channel.send('Please do not upload any files other than images or videos. For large blocks of code, use CodePen or PasteBin.');
+        message.channel.send(
+          'Please do not upload any files other than images or videos. For large blocks of code, use CodePen or PasteBin.'
+        );
       }
     }
     if (message.content.startsWith(config.PREFIX)) {
