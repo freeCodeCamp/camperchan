6313c1710c415c7de4c4968652f3c277a2259335
brad taniguchi <bradtaniguchi@gmail.com>
2020-06-17 23:12:05 -0700

    Fix lint warnings in add-formatting

--- src/commands/add-formatting.ts
+++ src/commands/add-formatting.ts
@@ -42,7 +42,10 @@ export async function addFormatting(message: Message): Promise<void> {
   console.log(languageGuesses);
 
   // TODO: this is due to being a version behind, will fix later
+  // See https://www.npmjs.com/package/@types/highlightjs for version 10
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   if (!(detectedWithHLJS as any).top.aliases) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     (detectedWithHLJS as any).top.aliases = [];
   }
 
