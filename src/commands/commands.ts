6f64a70a009f22dc852a2b736bf0871a27364ddb
brad taniguchi <bradtaniguchi@gmail.com>
2020-06-18 20:29:09 -0700

    Update coc.js to TS

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -2,5 +2,6 @@ import { CommandDef } from './command-def';
 import { eightBall } from './eightball';
 import { help } from './help';
 import { stats } from './stats';
+import { coc } from './coc';
 
-export const COMMANDS: Array<CommandDef> = [eightBall, help, stats];
+export const COMMANDS: Array<CommandDef> = [eightBall, help, stats, coc];
