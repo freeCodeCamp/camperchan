6a421c79d8e443c89bb82998576c79366450a09e
nhcarrigan <nhcarrigan@gmail.com>
2020-06-30 11:05:21 -0700

    Apply review comments

--- src/commands/forum.ts
+++ src/commands/forum.ts
@@ -6,34 +6,22 @@ export const forum: CommandDef = {
   prefix: 'forum',
   description: 'Gets the recent forum activity.',
   command: async (message: Message): Promise<void> => {
-    const data = await fetch('https://forum.freecodecamp.org/latest.json');
-    const parsed = await data.json();
-
-    const forumEmbed: MessageEmbed = new MessageEmbed()
-      .setTitle('Latest Forum Activity')
-      .setDescription('Here are the five most recent posts.')
-      .addFields(
-        {
-          name: `${parsed.topic_list.topics[0].title}`,
-          value: `[${parsed.topic_list.topics[0].last_poster_username} replied on ${parsed.topic_list.topics[0].last_posted_at}](https://forum.freecodecamp.org/t/${parsed.topic_list.topics[0].id})`
-        },
-        {
-          name: `${parsed.topic_list.topics[1].title}`,
-          value: `[${parsed.topic_list.topics[1].last_poster_username} replied on ${parsed.topic_list.topics[0].last_posted_at}](https://forum.freecodecamp.org/t/${parsed.topic_list.topics[1].id})`
-        },
-        {
-          name: `${parsed.topic_list.topics[2].title}`,
-          value: `[${parsed.topic_list.topics[2].last_poster_username} replied on ${parsed.topic_list.topics[0].last_posted_at}](https://forum.freecodecamp.org/t/${parsed.topic_list.topics[2].id})`
-        },
-        {
-          name: `${parsed.topic_list.topics[3].title}`,
-          value: `[${parsed.topic_list.topics[3].last_poster_username} replied on ${parsed.topic_list.topics[0].last_posted_at}](https://forum.freecodecamp.org/t/${parsed.topic_list.topics[3].id})`
-        },
-        {
-          name: `${parsed.topic_list.topics[4].title}`,
-          value: `[${parsed.topic_list.topics[4].last_poster_username} replied on ${parsed.topic_list.topics[0].last_posted_at}](https://forum.freecodecamp.org/t/${parsed.topic_list.topics[4].id})`
-        }
+    try {
+      const data = await fetch('https://forum.freecodecamp.org/latest.json');
+      const parsed = await data.json();
+      const result = parsed.topic_list.topics.slice(0, 5);
+      const forumEmbed: MessageEmbed = new MessageEmbed()
+        .setTitle('Latest Forum Activity')
+        .setDescription('Here are the five most recent posts.');
+      result.forEach((el: Record<string, unknown>) =>
+        forumEmbed.addFields({
+          name: `${el.title}`,
+          value: `[${el.last_poster_username} replied on ${el.last_posted_at}](https://forum.freecodecamp.org/t/${el.id})`
+        })
       );
-    message.channel.send(forumEmbed).catch((e) => console.error(e));
+      message.channel.send(forumEmbed);
+    } catch (error) {
+      console.error(error);
+    }
   }
 };
