37304ed48390cc1caad6b100a681a0b194419b5c
Joshua Pelealu <45566099+JoshuaPelealu@users.noreply.github.com>
2020-07-02 17:56:30 -0400

    Utilized luxon. Cleaner code

--- src/utilities/bot-online-time.ts
+++ src/utilities/bot-online-time.ts
@@ -1,12 +1,8 @@
-export function getBotOnlineAt(): string {
-  const date = new Date();
-  const hour = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours();
-  const mins =
-    date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes();
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  const timeZ = /\((.*)\)/.exec(new Date().toString())![1];
-  const tz = timeZ.match(/[A-Z]+/g)?.join('');
-  const am_pm = hour >= 12 ? 'PM' : 'AM';
+import { DateTime } from 'luxon';
 
-  return `${hour}:${mins} ${am_pm} ${tz}`;
+export function getBotOnlineAt(): string {
+  const dt = DateTime.local();
+  const format = dt.toLocaleString(DateTime.TIME_WITH_SHORT_OFFSET);
+
+  return format;
 }
