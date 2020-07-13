4ca11047277fc80f88c1a7cc6fb3a71956cdd20b
nhcarrigan <nhcarrigan@gmail.com>
2020-07-13 09:32:19 -0700

    Change trivia to .json

--- src/commands/trivia.ts
+++ src/commands/trivia.ts
@@ -1,20 +1,20 @@
 import { CommandDef } from './command-def';
-import { triviaFacts } from '../APIs/trivia-facts';
+import facts from '../APIs/trivia-facts.json';
 import { MessageEmbed } from 'discord.js';
 
 export const triviaCommand: CommandDef = {
   prefix: 'trivia',
   description: 'Returns a random bit of trivia!',
   command: (message) => {
-    const random = Math.floor(Math.random() * triviaFacts.length);
+    const random = Math.floor(Math.random() * facts.length);
     const triviaEmbed = new MessageEmbed().setTitle('Random Fact!').addFields(
       {
         name: 'Fact',
-        value: `${triviaFacts[random].fact}`
+        value: `${facts[random].fact}`
       },
       {
         name: 'Source',
-        value: `${triviaFacts[random].source}`
+        value: `${facts[random].source}`
       }
     );
     message.channel.send(triviaEmbed);
