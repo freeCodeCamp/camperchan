c292731dd69a320b9600f31c3b0cef6eb7cca701
nhcarrigan <nhcarrigan@gmail.com>
2020-07-31 10:47:36 -0700

    Refactor command - now focuses only on prior suspension

--- src/commands/user.ts
+++ src/commands/user.ts
@@ -11,26 +11,25 @@ export const userCommand: CommandDef = {
       console.log('No user provided.');
       return;
     }
+    if (!message.member?.hasPermission('KICK_MEMBERS')) {
+      console.log('Invalid permissions');
+      return;
+    }
+    if (!config.MONGO_URI) {
+      console.log('No database configured');
+      return;
+    }
+    const userSuspend: UserSuspend | null = await userSuspendModel.findOne({
+      userId: user.id
+    });
+    let suspended = userSuspend?.suspended;
+    if (!suspended) {
+      suspended = false;
+    }
     const userEmbed = new MessageEmbed()
       .setTitle(user.nickname || user.user.username)
       .setDescription('Here is the available data on them.')
-      .addFields(
-        { name: 'Username', value: user.user.username },
-        {
-          name: 'Roles',
-          value: user.roles.cache.map((role) => role.name).join(', ')
-        }
-      );
-    let suspended;
-    if (config.MONGO_URI) {
-      const userSuspend: UserSuspend | null = await userSuspendModel.findOne({
-        userId: user.id
-      });
-      suspended = userSuspend?.suspended;
-      if (suspended && message.member?.hasPermission('KICK_MEMBERS')) {
-        userEmbed.addFields({ name: 'Prior Suspension?', value: suspended });
-      }
-    }
+      .addFields({ name: 'Prior Suspension?', value: suspended });
     message.channel.send(userEmbed);
   }
 };
