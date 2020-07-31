a0172a42b5c55c1acdb51dc23e46f184da3638cb
Nicholas Carrigan <nhcarrigan@gmail.com>
2020-07-31 14:51:17 -0700

    Feature/90/add user command v2 (#107)
    - Add new `user` command to be available for admins to check on previously suspended user status.
    
    Closes #90

---
+++ src/commands/user.ts
@@ -0,0 +1,32 @@
+import { CommandDef } from './command-def';
+import { UserSuspend, userSuspendModel } from '../APIs/mongo-suspend';
+import { MessageEmbed } from 'discord.js';
+
+export const userCommand: CommandDef = {
+  prefix: 'user',
+  description: "Get data on a current user. Use the syntax 'user [username]'.",
+  command: async (message, { config }) => {
+    const user = message.mentions.members?.first();
+    if (!user) {
+      console.log('No user provided.');
+      return;
+    }
+    if (!message.member?.hasPermission('KICK_MEMBERS')) {
+      console.log('Invalid permissions');
+      return;
+    }
+    if (!config.MONGO_URI) {
+      console.log('No database configured');
+      return;
+    }
+    const userSuspend: UserSuspend | null = await userSuspendModel.findOne({
+      userId: user.id
+    });
+    const suspended = !!userSuspend?.suspended;
+    const userEmbed = new MessageEmbed()
+      .setTitle(user.nickname || user.user.username)
+      .setDescription('Here is the available data on them.')
+      .addFields({ name: 'Prior Suspension?', value: suspended });
+    message.channel.send(userEmbed);
+  }
+};
