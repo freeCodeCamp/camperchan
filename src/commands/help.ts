143cb37cd6cab72b724fa7bda5317d80940fa377
Joshua Pelealu <45566099+JoshuaPelealu@users.noreply.github.com>
2020-08-01 20:13:30 -0400

    Update Help Command

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,10 +1,11 @@
-import { MessageEmbedOptions } from 'discord.js';
+import { MessageEmbed } from 'discord.js';
 import { CommandDef } from './command-def';
 import { COMMANDS } from './commands';
 
 export const help: CommandDef = {
   prefix: 'help',
   description: 'Get the commands currently available with this bot',
+  usage: 'help [command]',
   /**
    * @name help
    * Displays currently available commands.
@@ -12,26 +13,51 @@ export const help: CommandDef = {
    * @param message the message provided
    * Now automatically adds new commands - make sure the .js file has a prefix and description,
    */
-  command: async (message) => {
-    const helpEmbed: MessageEmbedOptions = {
-      color: '#0099FF',
-      title: 'Bot Information',
-      description:
-        'Hello! I am a test bot created by bradtaniguchi and members of ' +
-        'FreeCodeCamp.org to experiment with the process of building a ' +
-        'Discord Bot. You can view my source code at ' +
-        ' https://github.com/bradtaniguchi/discord-bot-test',
-      fields: [],
-      footer: { text: 'I am not affiliated with FreeCodeCamp in any way.' }
-    };
-    COMMANDS.forEach((commandDef) => {
-      helpEmbed.fields?.push({
-        name: commandDef.prefix,
-        value: commandDef.description.substring(0, 1023),
-        inline: false
-      });
-    });
-    await message.author.send({ embed: helpEmbed });
-    message.channel.send('Help message sent!');
+  command: async (message, { config }) => {
+    const param = message.content.split(' ')[2];
+    if (param == undefined) {
+      const helpEmbed: MessageEmbed = new MessageEmbed()
+        .setColor('#0099FF')
+        .setTitle('Help Command')
+        .addField(
+          'Bot Information',
+          'Hello! I am a test bot created by bradtaniguchi and members of ' +
+            'FreeCodeCamp.org to experiment with the process of building a ' +
+            'Discord Bot. You can view my [source code here](https://github.com/bradtaniguchi/discord-bot-test)'
+        )
+        // .addField(
+        //   'Commands',
+        //   `Hey there ${message.author}, here is the full list of all my commands.\n` +
+        //     `For more information about each command, use \`${config.PREFIX} help <command>\``
+        // )
+        .addField(
+          'List of Commands',
+          COMMANDS.sort((a, b) =>
+            a.prefix.toUpperCase() > b.prefix.toUpperCase() ? 1 : -1
+          )
+            .map((command, i) => {
+              if ((i + 1) % 4 === 0) {
+                return `\`${command.prefix}\n\``;
+              } else return `\`${command.prefix}\``;
+            })
+            .join(' ') +
+            '\n \n' +
+            `For more information about each command, use \`${config.PREFIX} help <command>\``
+        )
+        .setFooter('I am not affiliated with FreeCodeCamp in any way.');
+      message.channel.send(helpEmbed);
+    } else {
+      const commandFound = COMMANDS.find((command) => command.prefix == param);
+      if (!commandFound)
+        return message.channel.send(
+          `Cannot find the command you specified -> \`${param}\``
+        );
+      const helpEmbed = new MessageEmbed()
+        .setColor('#0099FF')
+        .setTitle(commandFound.usage)
+        .addField('Description', `\`${commandFound.description}\``)
+        .addField('\u200b', 'Required: `<>` | Optional: `[]`');
+      message.channel.send(helpEmbed);
+    }
   }
 };
