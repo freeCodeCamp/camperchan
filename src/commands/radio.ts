e581e9600f2084cfe5f94872d75d3152e1e40ce2
nhcarrigan <nhcarrigan@gmail.com>
2020-08-10 12:17:23 -0700

    Add pause feature

--- src/commands/radio.ts
+++ src/commands/radio.ts
@@ -5,6 +5,7 @@ export const radio: CommandDef = {
   description: 'Play radio!',
   usage: 'radio',
   command: async (message) => {
+    const cmdArguments = message.content.split(' ')[2];
     const connection = await message.member?.voice.channel?.join();
     const dispatcher = connection?.play(
       'https://coderadio-relay.freecodecamp.org/radio/8010/radio.mp3'
@@ -12,5 +13,8 @@ export const radio: CommandDef = {
     dispatcher?.on('start', () => {
       message.channel.send('Playing!');
     });
+    if (cmdArguments === 'pause') {
+      dispatcher?.pause();
+    }
   }
 };
