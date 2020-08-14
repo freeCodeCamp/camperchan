4f20964facc859bcade1d23a9023f7090ea12502
Twaha Rahman <mahi6703890@gmail.com>
2020-08-14 12:45:55 +0600

    Fix tests (make regexs flexible)

--- src/tests/bot-online-time.test.ts
+++ src/tests/bot-online-time.test.ts
@@ -6,15 +6,17 @@ describe('botOnlineTime tests', () => {
   });
 
   it('Should return colon sparated Hour:Minute:Second', () => {
-    expect(getBotOnlineAt()).toMatch(/[0-9]{2}:[0-9]{2}:[0-9]{2}/g);
+    expect(getBotOnlineAt()).toMatch(
+      /([0-9][0-9]|[0-9]):([0-9][0-9]|[0-9]):([0-9][0-9]|[0-9])/g
+    );
   });
 
   it('Should return time with AM or PM format', () => {
     expect(getBotOnlineAt()).toMatch(/(P|A)M/g);
   });
 
-  it('Should return time format with GMT', () => {
-    expect(getBotOnlineAt()).toMatch(/GMT(\+|-)[0-9]/g);
+  it('Should return time format with standard time zones', () => {
+    expect(getBotOnlineAt()).toMatch(/(GMT(\+|-)[0-9])|UTC/g);
   });
 
   it('Should return Time, AM/PM and standard time zone separated by space', () => {
