76eff16b267bbf1bb80e6154511e5aa92ae65017
Twaha Rahman <39026437+Twaha-Rahman@users.noreply.github.com>
2020-08-14 12:55:34 +0600

    Add tests for some utility functions (#141)
    - Add tests for some utility functions

---
+++ src/tests/bot-online-time.test.ts
@@ -0,0 +1,25 @@
+import { getBotOnlineAt } from '../utilities/bot-online-time';
+
+describe('botOnlineTime tests', () => {
+  it('Should return string', () => {
+    expect(getBotOnlineAt()).toBeDefined();
+  });
+
+  it('Should return colon sparated Hour:Minute:Second', () => {
+    expect(getBotOnlineAt()).toMatch(
+      /([0-9][0-9]|[0-9]):([0-9][0-9]|[0-9]):([0-9][0-9]|[0-9])/g
+    );
+  });
+
+  it('Should return time with AM or PM format', () => {
+    expect(getBotOnlineAt()).toMatch(/(P|A)M/g);
+  });
+
+  it('Should return time format with standard time zones', () => {
+    expect(getBotOnlineAt()).toMatch(/(GMT(\+|-)[0-9])|UTC/g);
+  });
+
+  it('Should return Time, AM/PM and standard time zone separated by space', () => {
+    expect(getBotOnlineAt()).toMatch(/ /g);
+  });
+});
