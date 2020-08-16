a463927f65130dae92303696577ab0ffb375c4ce
Brad <bradtaniguchi@gmail.com>
2020-08-16 19:48:48 +0000

    Update console calls in close commands

--- src/commands/close.ts
+++ src/commands/close.ts
@@ -1,6 +1,7 @@
 import { CommandDef } from './command-def';
 import { TextChannel, MessageEmbed } from 'discord.js';
 import { oneLine } from 'common-tags';
+import { logger } from '../utilities/logger';
 
 export const closeCommand: CommandDef = {
   prefix: 'close',
@@ -12,29 +13,38 @@ export const closeCommand: CommandDef = {
   usage: 'close [accepted | denied] [user]',
   command: async (message, { config }): Promise<void> => {
     try {
-      if (!message.member?.hasPermission('MANAGE_CHANNELS'))
-        return console.log(
+      if (!message.member?.hasPermission('MANAGE_CHANNELS')) {
+        logger.warn(
           `${message.author.username} did not have the correct permissions.`
         );
+        return;
+      }
       const flag = message.content.split(' ')[2];
-      if (flag !== 'accepted' && flag !== 'denied')
-        return console.log('invalid paramater');
+      if (flag !== 'accepted' && flag !== 'denied') {
+        logger.warn('invalid paramater');
+        return;
+      }
       const target = message.channel as TextChannel;
       //check for log channel
       const log = message.guild?.channels.cache.find(
         (channel) => channel.name === config.LOG_MSG_CHANNEL
       ) as TextChannel;
       //check for user permissions
-      if (!message.member?.hasPermission('MANAGE_CHANNELS'))
-        return console.log('Missing permissions.');
-
-      if (!log) return console.log('Log channel not found.');
+      if (!message.member?.hasPermission('MANAGE_CHANNELS')) {
+        logger.warn('Missing permissions.');
+        return;
+      }
+      if (!log) {
+        logger.warn('Log channel not found.');
+        return;
+      }
 
       if (
         !target.name.includes('suspended') ||
         !target.parent?.name.includes(config.SUSPEND_CATEGORY)
       ) {
-        return console.log('Cannot delete non-temporary channel');
+        logger.warn('Cannot delete non-temporary channel');
+        return;
       }
 
       let status = 'The appeal was not approved.';
@@ -43,7 +53,7 @@ export const closeCommand: CommandDef = {
           (role) => role.name == config.SUSPEND_ROLE
         );
         if (!suspend) {
-          console.log('Cannot find suspend role');
+          logger.warn('Cannot find suspend role');
         } else {
           await message.mentions.members.first()?.roles.remove([suspend]);
           status = `The appeal was approved and ${message.mentions.members.first()}'s access was restored.`;
@@ -58,7 +68,7 @@ export const closeCommand: CommandDef = {
         .addFields({ name: 'Decision', value: status });
       await log.send(deleteEmbed);
     } catch (error) {
-      console.error(error);
+      logger.error(error);
     }
   }
 };
