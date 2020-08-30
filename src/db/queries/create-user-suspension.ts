36c57b42cc1d8086b20a656eb5fef09b159a7d41
bradtaniguchi <bradtaniguchi@gmail.com>
2020-08-30 00:43:33 -0700

    Add create-user-supension utility function
    - this will create/update properties within the database.

--- src/db/queries/create-user-suspension.ts
+++ src/db/queries/create-user-suspension.ts
@@ -1,47 +1,7 @@
-import { User, GuildMember } from 'discord.js';
+import { GuildMember, User } from 'discord.js';
 import { models } from '../models';
 import { User as DbUser } from '../user';
-
-/**
- * Creates a user-suspension for the given user.
- */
-export const createUserSuspension = async ({
-  user,
-  moderator
-}: {
-  /**
-   * The user who to create a user-suspension for.
-   * This function will automatically create a user entry
-   * if there isn't one.
-   */
-  user: GuildMember;
-  /**
-   * The discord user that created this suspension,
-   * which is the moderator, or author of the command.
-   */
-  moderator: User;
-}): Promise<void> => {
-  const existingUser = await models.users.findOne({
-    _id: user.id
-  });
-
-  const dbUser = existingUser
-    ? await updateExistingIdentity({
-        existingUser,
-        user
-      })
-    : await models.users.create({
-        _id: user.id,
-        identities: [
-          // create the first entry of identities
-          {
-            nickname: user.nickname || '',
-            username: user.user.username,
-            createdAt: new Date()
-          }
-        ]
-      });
-};
+import { UserSuspension } from '../user-suspension';
 
 /**
  * Update the existing identity within the existing user.
@@ -68,4 +28,77 @@ const updateExistingIdentity = async ({
     createdAt: new Date()
   });
   return existingUser.save();
+};
+
+/**
+ * Creates a user-suspension for the given user.
+ */
+export const createUserSuspension = async ({
+  reason,
+  user,
+  moderator
+}: {
+  /**
+   * The reason the user is suspended.
+   */
+  reason: string;
+  /**
+   * The user who to create a user-suspension for.
+   * This function will automatically create a user entry
+   * if there isn't one.
+   */
+  user: GuildMember;
+  /**
+   * The discord user that created this suspension,
+   * which is the moderator, or author of the command.
+   */
+  moderator: User;
+}): Promise<{
+  /**
+   * The user updated/created
+   */
+  dbUser: DbUser;
+  /**
+   * The suspension created
+   */
+  userSuspension: UserSuspension;
+}> => {
+  const existingUser = await models.users.findOne({
+    _id: user.id
+  });
+
+  const manageUser = async (): Promise<DbUser> =>
+    existingUser
+      ? await updateExistingIdentity({
+          existingUser,
+          user
+        })
+      : await models.users.create({
+          _id: user.id,
+          identities: [
+            // create the first entry of identities
+            {
+              nickname: user.nickname || '',
+              username: user.user.username,
+              createdAt: new Date()
+            }
+          ]
+        });
+  const createSuspension = (): Promise<UserSuspension> =>
+    models.user_suspensions.create({
+      reason,
+      user: user.id,
+      moderator: {
+        _id: moderator.id,
+        username: moderator.username
+      },
+      createdAt: new Date()
+    });
+
+  const [dbUser, userSuspension] = await Promise.all([
+    manageUser(),
+    createSuspension()
+  ]);
+
+  return { dbUser, userSuspension };
 };
