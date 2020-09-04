8b048cb4e636130e796e4ca2aa420f9ed82e5cfc
Twaha Rahman <mahi6703890@gmail.com>
2020-09-05 01:52:00 +0600

    Add type and number field to embed

--- src/utilities/issue-embed-generator.ts
+++ src/utilities/issue-embed-generator.ts
@@ -2,12 +2,26 @@ import { MessageEmbed } from 'discord.js';
 import { IssueData } from '../APIs/issue-def';
 
 export function issueEmbedGenerator(issueData: IssueData): MessageEmbed {
-  const { title, user, comments, state, html_url } = issueData;
+  const {
+    title,
+    user,
+    comments,
+    state,
+    html_url,
+    pull_request,
+    number
+  } = issueData;
   const bannerImageLink =
     'https://camo.githubusercontent.com/60c67cf9ac2db30d478d21755289c423e1f985c6/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f66726565636f646563616d702f776964652d736f6369616c2d62616e6e65722e706e67';
+  let embedType = 'Issue';
+  if (pull_request) {
+    embedType = 'Pull Request';
+  }
 
   const issueEmbed = new MessageEmbed()
     .setTitle(title)
+    .addField('Type', embedType, true)
+    .addField('ID', `#${number}`, true)
     .addField('Comments', comments.toString(), true)
     .addField('Status', state.toUpperCase(), true)
     .setAuthor(user.login, user.avatar_url, user.url)
