ce5593828efab5f53f559db89fb78d58486701bf
Naomi Carrigan <nhcarrigan@gmail.com>
2022-07-20 10:19:28 -0700

    feat: add canned response system (#227)

---
+++ src/commands/tags.ts
@@ -0,0 +1,66 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { GuildMember } from "discord.js";
+
+import { Tags } from "../config/Tags";
+import { Command } from "../interfaces/Command";
+import { errorHandler } from "../utils/errorHandler";
+import { isModerator } from "../utils/isModerator";
+
+export const tags: Command = {
+  data: new SlashCommandBuilder()
+    .setName("tags")
+    .setDescription("Display a commonly used canned response.")
+    .addStringOption((option) =>
+      option
+        .setName("name")
+        .setDescription("The name of the tag to display.")
+        .setRequired(true)
+    )
+    .addUserOption((option) =>
+      option
+        .setName("user")
+        .setDescription("User to ping with the tag response?")
+    ),
+  run: async (bot, interaction) => {
+    try {
+      const { member } = interaction;
+
+      if (!member) {
+        await interaction.reply({
+          content: "You must be in a guild to use this command.",
+          ephemeral: true,
+        });
+        return;
+      }
+
+      if (!isModerator(member as GuildMember)) {
+        await interaction.reply({
+          content: "You must be a moderator to use this command.",
+          ephemeral: true,
+        });
+        return;
+      }
+
+      const name = interaction.options.getString("name", true);
+      const targetTag = Tags.find((tag) => tag.name === name);
+      const user = interaction.options.getUser("user");
+
+      if (!targetTag) {
+        await interaction.reply({
+          content: "That tag does not exist.",
+          ephemeral: true,
+        });
+        return;
+      }
+
+      await interaction.reply({
+        content: user
+          ? `Hello <@!${user.id}>!\n\n${targetTag.message}`
+          : targetTag.message,
+      });
+    } catch (err) {
+      await errorHandler(bot, err);
+      await interaction.reply("Something went wrong.");
+    }
+  },
+};
---
+++ src/config/Tags.ts
@@ -0,0 +1,19 @@
+import { Tag } from "../interfaces/Tag";
+
+export const Tags: Tag[] = [
+  {
+    name: "picture",
+    message:
+      "Pictures of code are hard to read and cannot be copied into a local IDE for debugging. Please send your actual code or a link to your repository, instead of a picture.",
+  },
+  {
+    name: "gist",
+    message:
+      "Large amounts of code are disruptive to chat. Instead, please send a link to your repository, or a link to a [GitHub gist](https://gist.github.com/), [PasteBin](https://pastebin.com/), or similar.",
+  },
+  {
+    name: "format",
+    message:
+      "Discord has support for code block syntax. Please repost your code using this syntax:\n\n\\`\\`\\`lang\ncode here\n\\`\\`\\`\n\nYou can replace `lang` with your code's language, such as `js`, to get syntax highlighting.",
+  },
+];
