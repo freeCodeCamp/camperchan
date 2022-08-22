fa3e45dc6b4256bb1e0fee335be689ca400f6546
Naomi Carrigan <nhcarrigan@gmail.com>
2022-08-22 16:25:29 -0700

    feat: log thread creations

--- src/events/handlers/handleThreadCreate.ts
+++ src/events/handlers/handleThreadCreate.ts
@@ -1,4 +1,4 @@
-import { ThreadChannel } from "discord.js";
+import { EmbedBuilder, ThreadChannel } from "discord.js";
 
 import { Camperbot } from "../../interfaces/Camperbot";
 import { errorHandler } from "../../utils/errorHandler";
@@ -15,6 +15,16 @@ export const handleThreadCreate = async (
 ) => {
   try {
     await thread.join();
+
+    const embed = new EmbedBuilder();
+    embed.setTitle("Thread Created");
+    embed.setDescription(`The thread <#${thread.id}> was created.`);
+    embed.addFields([
+      { name: "Created By:", value: `<@!${thread.ownerId}>`, inline: true },
+      { name: "Channel:", value: `<#${thread.parentId}>`, inline: true },
+    ]);
+
+    await Bot.config.mod_hook.send({ embeds: [embed] });
   } catch (err) {
     await errorHandler(Bot, err);
   }
