94d3684972ad615c24f74048f04288d897bbd116
Naomi Carrigan <nhcarrigan@gmail.com>
2022-12-15 18:49:32 -0800

    feat: add better join/leave logging

---
+++ src/events/handlers/handleMemberAdd.ts
@@ -0,0 +1,37 @@
+import { EmbedBuilder } from "@discordjs/builders";
+import { GuildMember, PartialGuildMember } from "discord.js";
+
+import { Camperbot } from "../../interfaces/Camperbot";
+import { errorHandler } from "../../utils/errorHandler";
+
+/**
+ * Logs a message to the debug hook when someone joins the server.
+ *
+ * @param {Camperbot} Bot The bot's Discord instance.
+ * @param {GuildMember | PartialGuildMember} member The member that joined the server.
+ */
+export const handleMemberAdd = async (
+  Bot: Camperbot,
+  member: GuildMember | PartialGuildMember
+) => {
+  try {
+    if (!member.user) {
+      return;
+    }
+    const embed = new EmbedBuilder();
+    embed.setTitle("Member Joined");
+    embed.setDescription("A member has joined the server~!");
+    embed.setAuthor({
+      name: member.user.tag,
+      iconURL: member.user.displayAvatarURL(),
+    });
+    embed.setFooter({
+      text: `ID: ${member.id}`,
+    });
+    await Bot.config.mod_hook.send({
+      embeds: [embed],
+    });
+  } catch (err) {
+    await errorHandler(Bot, err);
+  }
+};
---
+++ src/events/handlers/handleMemberRemove.ts
@@ -0,0 +1,44 @@
+import { EmbedBuilder } from "@discordjs/builders";
+import { GuildMember, PartialGuildMember } from "discord.js";
+
+import { Camperbot } from "../../interfaces/Camperbot";
+import { errorHandler } from "../../utils/errorHandler";
+
+/**
+ * Logs a message to the debug hook when someone leaves the server.
+ *
+ * @param {Camperbot} Bot The bot's Discord instance.
+ * @param {GuildMember | PartialGuildMember} member The member that left the server.
+ */
+export const handleMemberRemove = async (
+  Bot: Camperbot,
+  member: GuildMember | PartialGuildMember
+) => {
+  try {
+    if (!member.user) {
+      return;
+    }
+    const embed = new EmbedBuilder();
+    embed.setTitle("Member Joined");
+    embed.setDescription("A member has left the server~!");
+    embed.addFields([
+      {
+        name: "Roles",
+        value:
+          member.roles?.cache.map((role) => role.name).join(", ") || "unknown",
+      },
+    ]);
+    embed.setAuthor({
+      name: member.user.tag,
+      iconURL: member.user.displayAvatarURL(),
+    });
+    embed.setFooter({
+      text: `ID: ${member.id}`,
+    });
+    await Bot.config.mod_hook.send({
+      embeds: [embed],
+    });
+  } catch (err) {
+    await errorHandler(Bot, err);
+  }
+};
