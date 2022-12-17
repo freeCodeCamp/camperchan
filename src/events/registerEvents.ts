4306bcd283fc06dd524927a62a6072ef3c08c0f2
Naomi Carrigan <nhcarrigan@gmail.com>
2022-12-17 12:02:08 -0800

    fix: update event name

--- src/events/registerEvents.ts
+++ src/events/registerEvents.ts
@@ -20,7 +20,7 @@ export const registerEvents = async (Bot: Camperbot) => {
     Bot.on("ready", async () => await handleReady(Bot));
     Bot.on("messageCreate", async (msg) => await handleMessageCreate(Bot, msg));
     Bot.on(
-      "messageEdit",
+      "messageUpdate",
       async (oldMsg, newMsg) => await handleMessageEdit(Bot, oldMsg, newMsg)
     );
     Bot.on("messageDelete", async (msg) => await handleMessageDelete(Bot, msg));
