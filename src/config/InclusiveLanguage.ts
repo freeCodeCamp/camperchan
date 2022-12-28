f6281c7d27cb5af8f041efeecba723e0cb4e88a9
Naomi Carrigan <nhcarrigan@gmail.com>
2022-12-28 13:02:26 -0800

    Revert "feat: add inclusive language reminders (#255)"
    This reverts commit e4670341e7ad4a4cf84bf87406d10e3d84a7d9a6.

--- src/config/InclusiveLanguage.ts
+++
@@ -1,8 +0,0 @@
-import { Config } from "alex";
-
-export const InclusiveLanguage: Config = {
-  profanitySureness: 1,
-  noBinary: true,
-  // AlexJS to ignore these grouped words https://github.com/retextjs/retext-equality/blob/main/rules.md
-  allow: ["he-she"],
-};
--- src/events/handlers/handleMessageCreate.ts
+++ src/events/handlers/handleMessageCreate.ts
@@ -4,7 +4,6 @@ import { join } from "path";
 import { ChannelType, Message } from "discord.js";
 
 import { Camperbot } from "../../interfaces/Camperbot";
-import { checkInclusiveLanguage } from "../../utils/checkInclusiveLanguage";
 
 /**
  * Handles the message create events.
@@ -13,25 +12,6 @@ import { checkInclusiveLanguage } from "../../utils/checkInclusiveLanguage";
  * @param {Message} message The message payload from Discord.
  */
 export const handleMessageCreate = async (Bot: Camperbot, message: Message) => {
-  const nonInclusiveWarnings = await checkInclusiveLanguage(Bot, message);
-
-  if (nonInclusiveWarnings.length && !message.author.bot) {
-    await message.reply({
-      content: `Hey there <@${
-        message.author.id
-      }>~! It looks like your message might be using language that isn't inclusive. Please consider these following suggestions:\n\n${nonInclusiveWarnings
-        .map(
-          (el) =>
-            `- ||${el.actual}||: ${el.reason.replace(/`/g, "||")}${
-              el.note ? " - " + el.note : ""
-            }`
-        )
-        .join(
-          "\n"
-        )}\n\nThank you for keeping our community safe and welcoming~!`,
-    });
-  }
-
   if (
     message.channel.type !== ChannelType.GuildText ||
     !message.channel.name.startsWith("private-")
