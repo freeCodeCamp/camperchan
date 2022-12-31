634e1957b598748be86ddaa09eb65a02de74da81
Naomi Carrigan <nhcarrigan@gmail.com>
2022-12-30 19:27:56 -0800

    fix: mount level listener

--- src/events/handlers/handleMessageCreate.ts
+++ src/events/handlers/handleMessageCreate.ts
@@ -4,6 +4,7 @@ import { join } from "path";
 import { ChannelType, Message } from "discord.js";
 
 import { Camperbot } from "../../interfaces/Camperbot";
+import { levelListener } from "../../modules/levelListener";
 
 /**
  * Handles the message create events.
@@ -12,6 +13,8 @@ import { Camperbot } from "../../interfaces/Camperbot";
  * @param {Message} message The message payload from Discord.
  */
 export const handleMessageCreate = async (Bot: Camperbot, message: Message) => {
+  await levelListener(Bot, message);
+
   if (
     message.channel.type !== ChannelType.GuildText ||
     !message.channel.name.startsWith("private-")
