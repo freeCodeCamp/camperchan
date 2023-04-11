dfe4d440f0a6b56e10bd5594cb9a051947ef90e3
Aaron <a.rcottrill521@gmail.com>
2023-04-11 12:47:28 -0400

    Clean up

--- src/utils/calculateMilliseconds.ts
+++ src/utils/calculateMilliseconds.ts
@@ -6,7 +6,10 @@
  * @param {string} unit The unit of time to convert to milliseconds.
  * @returns {number} The number of milliseconds.
  */
-export const calculateMilliseconds = (value: number, unit: string): number => {
+export const calculateMilliseconds = (
+  value: number,
+  unit: "seconds" | "minutes" | "hours" | "days" | "weeks"
+): number => {
   switch (unit.toLowerCase()) {
     case "seconds":
       return value * 1000;
