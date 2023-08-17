04a5e1a5e0ac578a89088fbd1a25132fd3976eca
Naomi Carrigan <nhcarrigan@gmail.com>
2023-08-17 11:47:16 -0700

    chore: encourage asking in Discord

--- src/config/PullComments.ts
+++ src/config/PullComments.ts
@@ -2,6 +2,6 @@ export const PullComments: { key: string; message: string }[] = [
   {
     key: "Don't Ping Maintainers",
     message:
-      "We realize you're looking to get help as soon as possible. Rather than pinging someone directly, which can be considered rude, would you mind sharing your question so maybe someone who is here now can help right away?",
+      "We realize you're looking to get help as soon as possible. Rather than pinging someone directly, which can be considered rude, would you mind [joining our Discord](https://discord.gg/KVUmVXA) and asking your question there? Someone might be more readily available to help.",
   },
 ];
