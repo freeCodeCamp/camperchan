5fb9e41ebf92909ff120820a8b59f60472f3005a
Naomi Carrigan <nhcarrigan@gmail.com>
2023-09-20 10:26:55 -0700

    feat: install testing package

--- test/commands/coc.spec.ts
+++ test/commands/coc.spec.ts
@@ -1,6 +1,46 @@
 import { assert } from "chai";
+import {
+  MockChannel,
+  MockChatInputCommandInteraction,
+  MockGuild,
+  MockMember,
+  MockUser,
+  MockWebhook,
+} from "discordjs-testing";
 
 import { coc } from "../../src/commands/coc";
+import { ChannelType } from "discord.js";
+
+const guild = new MockGuild({
+  name: "Test Guild",
+});
+const bot = new MockUser({
+  username: "Test Bot",
+  avatar: "test",
+  discriminator: 1234,
+  bot: true,
+  system: false,
+});
+const user = new MockUser({
+  username: "Test User",
+  avatar: "test",
+  discriminator: 1234,
+  bot: false,
+  system: false,
+});
+const member = new MockMember({
+  guild,
+  user,
+});
+const channel = new MockChannel({
+  name: "test-channel",
+  guild,
+  type: ChannelType.GuildText,
+});
+const debugHook = new MockWebhook({
+  channel,
+  user: bot,
+});
 
 suite("coc command", () => {
   test("coc command is defined", () => {
