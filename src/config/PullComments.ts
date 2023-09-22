85bf65b814b403f57a92f9cab77e0bb927632da8
Naomi Carrigan <nhcarrigan@gmail.com>
2023-09-22 08:19:12 -0700

    feat: add command to close issues or prs (#427)
    * feat: add command to close issues or prs
    
    * feat: do not close already closed issues/prs
    
    * fix(tools): update tests
    
    * chore: tweak repo description
    
    Co-authored-by: Mrugesh Mohapatra <1884376+raisedadead@users.noreply.github.com>
    
    * fix(tools): update tests
    
    ---------
    
    Co-authored-by: Mrugesh Mohapatra <1884376+raisedadead@users.noreply.github.com>

--- src/config/PullComments.ts
+++ src/config/PullComments.ts
@@ -5,3 +5,16 @@ export const PullComments: { key: string; message: string }[] = [
       "We realize you're looking to get help as soon as possible. Rather than pinging someone directly, which can be considered rude, would you mind [joining our Discord](https://discord.gg/KVUmVXA) and asking your question there? Someone might be more readily available to help.",
   },
 ];
+
+export const PullClose = `Thank you for opening this pull request.
+
+This is a standard message notifying you that we've reviewed your pull request and have decided not to merge it. We would welcome future pull requests from you.
+
+Thank you and happy coding.
+`;
+
+export const IssueClose = `Thank you for reporting this issue.
+
+This is a standard message notifying you that this issue is not on our roadmap at this time, and not something we plan to implement.
+
+If you think we're wrong in closing this issue, please request for it to be reopened and add further clarification. Thank you and happy coding.`;
--- test/__mocks__/statics.ts
+++ test/__mocks__/statics.ts
@@ -1,5 +1,6 @@
 export const CommandNames = [
   "ban",
+  "close",
   "coc",
   "contribute",
   "forum",
