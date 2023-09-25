5c03709698c434913936cbb7423753df1996b861
Naomi Carrigan <nhcarrigan@gmail.com>
2023-09-25 08:57:53 -0700

    feat: test ban command

--- test/commands/ban.spec.ts
+++ test/commands/ban.spec.ts
@@ -1,21 +1,78 @@
 import { assert } from "chai";
-import { ApplicationCommandOptionType, PermissionFlagsBits } from "discord.js";
+import {
+  ApplicationCommandOptionType,
+  ChannelType,
+  PermissionFlagsBits,
+} from "discord.js";
+import {
+  MockChannel,
+  MockChatInputCommandInteraction,
+  MockGuild,
+  MockMember,
+  MockUser,
+  MockWebhook,
+} from "discordjs-testing";
 
 import { ban } from "../../src/commands/ban";
+import { Database } from "../__mocks__/Database.mock";
 
-suite("ban command", () => {
-  test("ban command is defined", () => {
+const db = new Database();
+const guild = new MockGuild({
+  name: "Test Guild",
+});
+const bot = new MockUser({
+  username: "Test Bot",
+  avatar: "test",
+  discriminator: 1234,
+  bot: true,
+  system: false,
+});
+const user = new MockUser({
+  username: "Test User",
+  avatar: "test",
+  discriminator: 1234,
+  bot: false,
+  system: false,
+});
+const banUser = new MockUser({
+  username: "Test Ban User",
+  avatar: "https://cdn.nhcarrigan.com/profile.png",
+  discriminator: 1234,
+  bot: false,
+  system: false,
+});
+const member = new MockMember({
+  guild,
+  user,
+});
+const banMember = new MockMember({
+  guild,
+  user: banUser,
+});
+banMember.bannable = true;
+const channel = new MockChannel({
+  name: "test-channel",
+  guild,
+  type: ChannelType.GuildText,
+});
+const debugHook = new MockWebhook({
+  channel,
+  user: bot,
+});
+
+suite.only("ban command", () => {
+  test("should be defined", () => {
     assert.isDefined(ban);
   });
 
-  test("ban is a command object", () => {
+  test("should be a command object", () => {
     assert.isDefined(ban.data);
     assert.isObject(ban.data);
     assert.isDefined(ban.run);
     assert.isFunction(ban.run);
   });
 
-  test("ban command has correct configuration", () => {
+  test("should have correct configuration", () => {
     assert.isTrue(ban.guildOnly, "ban command is not guild only");
     assert.include(
       ban.requiredPermissions,
@@ -24,7 +81,7 @@ suite("ban command", () => {
     );
   });
 
-  test("ban command has correct data", () => {
+  test("should have correct data", () => {
     assert.equal(ban.data.name, "ban");
     assert.equal(ban.data.description, "Bans a user from the server.");
     assert.lengthOf(ban.data.options, 2);
@@ -38,5 +95,94 @@ suite("ban command", () => {
     assert.equal(reasonOption.description, "The reason for banning the user.");
     assert.equal(reasonOption.type, ApplicationCommandOptionType.String);
     assert.isTrue(reasonOption.required);
+  });
+
+  test("should not allow banning self", async () => {
+    const command = new MockChatInputCommandInteraction({
+      commandName: "ban",
+      guild,
+      bot,
+      user,
+      member,
+      channel,
+      options: [
+        {
+          name: "target",
+          type: ApplicationCommandOptionType.User,
+          value: user,
+        },
+        {
+          name: "reason",
+          type: ApplicationCommandOptionType.String,
+          value: "Banning myself hehe",
+        },
+      ],
+    });
+    await ban.run(bot as never, command as never);
+    assert.lengthOf(command.replies, 1);
+    assert.equal(
+      command.replies[0].content,
+      "You cannot ban yourself.",
+      "Incorrect reply"
+    );
+  });
+
+  test("should not allow banning bot", async () => {
+    const command = new MockChatInputCommandInteraction({
+      commandName: "ban",
+      guild,
+      bot,
+      user,
+      member,
+      channel,
+      options: [
+        {
+          name: "target",
+          type: ApplicationCommandOptionType.User,
+          value: bot,
+        },
+        {
+          name: "reason",
+          type: ApplicationCommandOptionType.String,
+          value: "Banning the bot hehe",
+        },
+      ],
+    });
+    await ban.run({ user: bot } as never, command as never);
+    assert.lengthOf(command.replies, 1);
+    assert.equal(
+      command.replies[0].content,
+      "You cannot ban the bot.",
+      "Incorrect reply"
+    );
+  });
+
+  test("should ban the member", async () => {
+    const command = new MockChatInputCommandInteraction({
+      commandName: "ban",
+      guild,
+      bot,
+      user,
+      member,
+      channel,
+      options: [
+        {
+          name: "target",
+          type: ApplicationCommandOptionType.User,
+          value: banUser,
+        },
+        {
+          name: "reason",
+          type: ApplicationCommandOptionType.String,
+          value: "Real ban?",
+        },
+      ],
+    });
+    await ban.run(
+      { user: bot, db, config: { mod_hook: debugHook } } as never,
+      command as never
+    );
+    assert.lengthOf(command.replies, 1);
+    assert.equal(command.replies[0].content, "They have been banned.");
   });
 });
