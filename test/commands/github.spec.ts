79eabec0b481c769e7277f115cbd51cf25a191b1
Naomi Carrigan <nhcarrigan@gmail.com>
2023-11-05 07:50:05 -0800

    feat: add labelling commands (#464)
    * feat: add labelling commands
    
    * feat(tools): update tests

--- test/commands/github.spec.ts
+++ test/commands/github.spec.ts
@@ -18,7 +18,7 @@ suite("github command", () => {
       "Commands related to managing github"
     );
     assert.isFalse(github.data.dm_permission);
-    assert.lengthOf(subcommands, 2);
+    assert.lengthOf(subcommands, 4);
   });
 
   test("has correct close", () => {
@@ -109,6 +109,88 @@ suite("github command", () => {
     assert.isTrue(comment?.options?.[2].required);
     assert.strictEqual(
       comment?.options?.[2].type,
+      ApplicationCommandOptionType.String
+    );
+  });
+
+  test("has correct add labels", () => {
+    const command = subcommands.find((sub) => sub.name === "add-labels");
+    assert.exists(command);
+    assert.strictEqual(command?.name, "add-labels");
+    assert.strictEqual(
+      command?.description,
+      "Specify a list of labels to add to an issue/PR. Existing labels will not be removed."
+    );
+    assert.lengthOf(command?.options || "hi", 3);
+    assert.strictEqual(command?.options?.[0].name, "repository");
+    assert.strictEqual(
+      command?.options?.[0].description,
+      "The name of the repository, under freeCodeCamp's GitHub org, to label"
+    );
+    assert.isTrue(command?.options?.[0].required);
+    assert.strictEqual(
+      command?.options?.[0].type,
+      ApplicationCommandOptionType.String
+    );
+    assert.strictEqual(command?.options?.[1].name, "number");
+    assert.strictEqual(
+      command?.options?.[1].description,
+      "The number of the pull request to label."
+    );
+    assert.isTrue(command?.options?.[1].required);
+    assert.strictEqual(
+      command?.options?.[1].type,
+      ApplicationCommandOptionType.Integer
+    );
+    assert.strictEqual(command?.options?.[2].name, "labels");
+    assert.strictEqual(
+      command?.options?.[2].description,
+      "The comma separated list of labels to add"
+    );
+    assert.isTrue(command?.options?.[2].required);
+    assert.strictEqual(
+      command?.options?.[2].type,
+      ApplicationCommandOptionType.String
+    );
+  });
+
+  test("has correct sync labels", () => {
+    const command = subcommands.find((sub) => sub.name === "sync-labels");
+    assert.exists(command);
+    assert.strictEqual(command?.name, "sync-labels");
+    assert.strictEqual(
+      command?.description,
+      "Specify the exact list of labels an issue/PR should have. Labels not on the list will be removed."
+    );
+    assert.lengthOf(command?.options || "hi", 3);
+    assert.strictEqual(command?.options?.[0].name, "repository");
+    assert.strictEqual(
+      command?.options?.[0].description,
+      "The name of the repository, under freeCodeCamp's GitHub org, to label"
+    );
+    assert.isTrue(command?.options?.[0].required);
+    assert.strictEqual(
+      command?.options?.[0].type,
+      ApplicationCommandOptionType.String
+    );
+    assert.strictEqual(command?.options?.[1].name, "number");
+    assert.strictEqual(
+      command?.options?.[1].description,
+      "The number of the pull request to label."
+    );
+    assert.isTrue(command?.options?.[1].required);
+    assert.strictEqual(
+      command?.options?.[1].type,
+      ApplicationCommandOptionType.Integer
+    );
+    assert.strictEqual(command?.options?.[2].name, "labels");
+    assert.strictEqual(
+      command?.options?.[2].description,
+      "The comma separated list of labels to sync"
+    );
+    assert.isTrue(command?.options?.[2].required);
+    assert.strictEqual(
+      command?.options?.[2].type,
       ApplicationCommandOptionType.String
     );
   });
---
+++ test/commands/subcommands/github/handleAddLabels.spec.ts
@@ -0,0 +1,38 @@
+import { assert } from "chai";
+import { PermissionFlagsBits } from "discord.js";
+
+import { handleAddLabels } from "../../../../src/commands/subcommands/github/handleAddLabels";
+
+suite("add labels handler", () => {
+  test("does not allow non-moderators permission", () => {
+    assert.isFalse(
+      handleAddLabels.permissionValidator({
+        permissions: new Set([PermissionFlagsBits.SendMessages]),
+      } as never)
+    );
+  });
+
+  test("allows moderate members permission", () => {
+    assert.isTrue(
+      handleAddLabels.permissionValidator({
+        permissions: new Set([PermissionFlagsBits.ModerateMembers]),
+      } as never)
+    );
+  });
+
+  test("allows kick members permission", () => {
+    assert.isTrue(
+      handleAddLabels.permissionValidator({
+        permissions: new Set([PermissionFlagsBits.KickMembers]),
+      } as never)
+    );
+  });
+
+  test("allows ban members permission", () => {
+    assert.isTrue(
+      handleAddLabels.permissionValidator({
+        permissions: new Set([PermissionFlagsBits.BanMembers]),
+      } as never)
+    );
+  });
+});
