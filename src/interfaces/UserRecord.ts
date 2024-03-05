96ec30e59f36105fca60142c7dd840e84434a542
Naomi Carrigan <nhcarrigan@gmail.com>
2024-03-04 17:39:35 -0800

    feat: track certification properties

--- src/interfaces/UserRecord.ts
+++ src/interfaces/UserRecord.ts
@@ -4,4 +4,24 @@
 export interface UserRecord {
   email: string;
   isDonating?: boolean;
+  isRespWebDesignCert?: boolean;
+  is2018DataVisCert?: boolean;
+  isFrontEndLibsCert?: boolean;
+  isJsAlgoDataStructCert?: boolean;
+  isApisMicroservicesCert?: boolean;
+  isInfosecQaCert?: boolean;
+  isQaCertV7?: boolean;
+  isInfosecCertV7?: boolean;
+  is2018FullStackCert?: boolean;
+  isFrontEndCert?: boolean;
+  isBackEndCert?: boolean;
+  isDataVisCert?: boolean;
+  isFullStackCert?: boolean;
+  isSciCompPyCertV7?: boolean;
+  isDataAnalysisPyCertV7?: boolean;
+  isMachineLearningPyCertV7?: boolean;
+  isRelationalDatabaseCertV8?: boolean;
+  isCollegeAlgebraPyCertV8?: boolean;
+  isFoundationalCSharpCertV8?: boolean;
+  isJsAlgoDataStructCertV8?: boolean;
 }
