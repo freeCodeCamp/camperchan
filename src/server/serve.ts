8a240292f8596f1c243c45b66c021c78122d2302
Naomi Carrigan <commits@nhcarrigan.com>
2024-10-16 12:01:29 -0700

    feat: add webhook endpointing for issues

--- src/server/serve.ts
+++ src/server/serve.ts
@@ -4,11 +4,16 @@
  * @author Naomi Carrigan
  */
 
+import { createHmac, timingSafeEqual } from "node:crypto";
 import { readFile } from "node:fs/promises";
-// eslint-disable-next-line import/default, import/no-named-as-default, import/no-named-as-default-member
+import { WebhookClient } from "discord.js";
 import fastify from "fastify";
 import { errorHandler } from "../utils/errorHandler.js";
 import type { ExtendedClient } from "../interfaces/extendedClient.js";
+
+const contributorHook = new WebhookClient({
+  url: process.env.CONTRIBUTOR_HOOK ?? "",
+});
 
 /**
  * Mounts a fastify server to allow for data analytics.
@@ -35,6 +40,64 @@ export const instantiateServer = async(
 
     server.get("/", (_request, response) => {
       response.status(200).send("Health Check!");
+    });
+
+    server.post("/github", async(request, response) => {
+      const githubSecret = process.env.GITHUB_WEBHOOK_SECRET;
+      if (githubSecret === undefined) {
+        await response.status(500).send("No secret provided.");
+        return;
+      }
+      const event = request.headers["x-github-event"];
+      const signature = request.headers["x-hub-signature-256"];
+      if (typeof event !== "string") {
+        await response.
+          status(400).
+          send(`Invalid value ${String(event)} for X-GitHub-Event`);
+        return;
+      }
+      if (typeof signature !== "string") {
+        await response.
+          status(400).
+          send(`Invalid value ${String(signature)} for X-Hub-Signature-256`);
+        return;
+      }
+      const sig = createHmac("sha256", githubSecret).
+        update(JSON.stringify(request.body)).
+        digest("hex");
+      const trusted = Buffer.from(`sha256=${sig}`, "ascii");
+      const sent = Buffer.from(signature, "ascii");
+      const safe = timingSafeEqual(trusted, sent);
+      if (!safe) {
+        await response.
+          status(401).
+          send("Failed to verify webhook secret hash.");
+        return;
+      }
+      // It's valid, so send an ok response immediately
+      await response.status(200).send("OK~!");
+
+      if (event === "issues") {
+        // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+        const { action, issue, label } = request.body as {
+          action: string;
+          // eslint-disable-next-line @typescript-eslint/naming-convention
+          issue:  { title: string; html_url: string; number: number };
+          label:  { name: string };
+        };
+        if (action === "labeled") {
+          if (label.name === "help wanted") {
+            await contributorHook.send({
+              content: `[#${issue.number.toString()} ${issue.title}](${issue.html_url}) is open for contribution!`,
+            });
+          }
+          if (label.name === "first timers only") {
+            await contributorHook.send({
+              content: `[#${issue.number.toString()} ${issue.title}](${issue.html_url}) is available for first-time contributors!`,
+            });
+          }
+        }
+      }
     });
 
     server.listen({ port: 1443 }, (error) => {
