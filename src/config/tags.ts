c46a4ce595ff7689ddc58990142a2181fec91df6
Rex Schrader <rex.schrader@gmail.com>
2025-04-07 15:02:03 -0700

    Update src/config/tags.ts
    Co-authored-by: Naomi <accounts+github@nhcarrigan.com>

--- src/config/tags.ts
+++ src/config/tags.ts
@@ -22,7 +22,7 @@ export const tags: Array<Tag> = [
       "Don't ask to ask, just ask\n<https://dontasktoask.com>. For details on how to ask a strong question, visit https://canary.discord.com/channels/692816967895220344/1033845883822948502",
     name: "How to ask a question",
   },
-    {
+  {
     message:
       "Please email donors@freecodecamp.org and provide the email you use to log into freecodecamp.org/learn, your donation receipt, and a brief explanation of your issue.",
     name: "Donation Problems",
